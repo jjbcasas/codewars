494faa1cd98486799e300d7a27245bf4df39add5
John Joseph Casas <casasjohnjoseph@gmail.com>
2024-12-12 23:52:01 -0800

    added new 6kyu challenge

---
+++ 6kyu/counting-duplicates.js
@@ -0,0 +1,44 @@
+// Description:
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+// My Solutions
+
+function duplicateCount(text){
+    let map = {}
+   let store = 0
+    
+    let letters = text.toLowerCase()
+    
+    //check the total times a character occured
+    for ( let char of letters ){
+      if (map[char]){
+        map[char] += 1
+      } else {
+        map[char] = 1
+      }
+    }
+    
+    //checking how many characters showed up more than once
+    for ( let count in map ){
+      if ( map[count] > 1){
+        store++
+      }
+    }
+    
+    // returning the value
+    if ( text.length === 0 ){
+      return 0
+    } else {
+      return store
+    }
+  }
